dcc40684a96cf48cc9440f6a0719581a0958fff9
Bryan Paronto <bparonto@gmail.com>
2018-01-21 00:42:23 -0600

    split and DRY up webpack configs

--- src/client/client.js
+++ src/client/client.js
@@ -3,4 +3,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './components/Home'
 
-ReactDOM.hydrate(<Home/>, document.getElementById('root'))
+ReactDOM.hydrate(<Home />, document.getElementById('root'))
